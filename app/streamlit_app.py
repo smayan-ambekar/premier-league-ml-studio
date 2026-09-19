823a35cd17ca4994506fa41f9e116a5030174418
Smayan Ambekar <smayanambekar007@gmail.com>
2026-09-19 17:41:49 +0530

    Initial Streamlit app skeleton

---
+++ app/streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Premier League ML Studio", page_icon = "⚽")
+
+st.title("Premier League ML Studio")
+st.write(
+    "This is starting point for a machine-learning application "
+    "covering player valuation, match prediction and player scouting."
+)
